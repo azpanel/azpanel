f7f6039a8dbed36658f686c611cf59924b0c8171
azpanel <100949601+azpanel@users.noreply.github.com>
2022-03-11 20:47:03 +0800

    add migration file

---
+++ database/migrations/20220311122419_storage_task_error.php
@@ -0,0 +1,35 @@
+<?php
+
+use think\migration\db\Column;
+use think\migration\Migrator;
+
+class StorageTaskError extends Migrator
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('task');
+        $table->addColumn('error', 'text', array('comment' => '任务报错', 'default' => null, 'null' => true, 'after' => 'current'))
+            ->update();
+    }
+}
