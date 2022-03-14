d1cdd13f460feb27b1381b544c27fe5fc379a88c
azpanel <100949601+azpanel@users.noreply.github.com>
2022-03-14 14:33:26 +0800

    better display of errors

--- app/controller/AdminLog.php
+++ app/controller/AdminLog.php
@@ -53,10 +53,13 @@ class AdminLog extends AdminBase
     {
         $log = Task::find($id);
         $total = json_decode($log->total, true);
+        $error = json_decode($log->error);
+        $error = json_encode($error, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
 
-        View::assign('count', 0);
         View::assign('log', $log);
+        View::assign('count', '0');
         View::assign('total', $total);
+        View::assign('error', $error);
         return View::fetch('../app/view/admin/log/task_details.html');
     }
 }
