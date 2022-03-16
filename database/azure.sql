dfc936975875678bd240196a21b4a51dd2fc8cf5
azpanel <100949601+azpanel@users.noreply.github.com>
2022-03-16 19:25:36 +0800

    modify the length of the email field

--- database/azure.sql
+++ database/azure.sql
@@ -140,7 +140,7 @@ DROP TABLE IF EXISTS `user`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `email` varchar(255) NOT NULL COMMENT '邮箱地址',
+  `email` varchar(128) NOT NULL COMMENT '邮箱地址',
   `passwd` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '密码',
   `status` int NOT NULL DEFAULT '1' COMMENT '账户状态',
   `notify_email` text COLLATE utf8mb4_general_ci COMMENT '用户通知邮箱',
