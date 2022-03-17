239657821c40c83d4bce4af2ecd54594d6ec0ccc
azpanel <100949601+azpanel@users.noreply.github.com>
2022-03-17 22:56:49 +0800

    add migration file

---
+++ database/migrations/20220317132409_quota.php
@@ -0,0 +1,38 @@
+<?php
+
+use think\migration\Migrator;
+use think\migration\db\Column;
+
+class Quota extends Migrator
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('azure');
+        $table->addColumn('reg_capacity', 'integer', array(
+            'comment' => '是否注册Capacity提供商',
+            'default' => '0',
+            'after' => 'providers_register')
+            )->update();
+    }
+}
