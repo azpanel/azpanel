2bff76759d6104f497c8afdc5095ad3566fc1304
azpanel <100949601+azpanel@users.noreply.github.com>
2022-05-28 21:07:55 +0800

    fix

--- app/controller/UserAzure.php
+++ app/controller/UserAzure.php
@@ -150,8 +150,8 @@ class UserAzure extends UserBase
         if (strlen($az_api_tenant_id) != 36) {
             return json(Tools::msg('0', '添加失败', 'tenant_id 长度应为36位'));
         }
-        if (strlen($az_api_secret) != 34 && strlen($az_api_secret) != 41) {
-            return json(Tools::msg('0', '添加失败', 'secret 长度应为36位或41位'));
+        if (strlen($az_api_secret) != 34 || strlen($az_api_secret) != 40) {
+            return json(Tools::msg('0', '添加失败', 'secret 长度应为36位或40位'));
         }
 
         $az_api = [
