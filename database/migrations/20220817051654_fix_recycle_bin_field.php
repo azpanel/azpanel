2d81958a18b8a84ce4401d8ba85b1bce82d345fc
azpanel <100949601+azpanel@users.noreply.github.com>
2022-08-17 13:19:48 +0800

    fix

---
+++ database/migrations/20220817051654_fix_recycle_bin_field.php
@@ -0,0 +1,36 @@
+<?php
+
+use think\migration\Migrator;
+use think\migration\db\Column;
+
+class FixRecycleBinField extends Migrator
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $users = $this->table('azure_recycle');
+        $users->changeColumn('bill_charges', 'float', ['comment' => '预估费用'])
+            ->changeColumn('life_cycle', 'float', ['comment' => '账户生命周期'])
+            ->save();
+    }
+}
