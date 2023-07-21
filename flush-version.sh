dfcaf1c5d0e5215a36933230f70901b2bffc404d
azpanel <100949601+azpanel@users.noreply.github.com>
2023-07-21 20:05:41 +0800

    add script

---
+++ flush-version.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+version=$(git log --format="%ct" | wc -l)
+big_v='1'
+medium_v='1'
+small_v=$(expr ${version} - 105)
+hash=$(git log -1 --format="%h")
+
+if [[ -e "/usr/local/php8.1/bin/php" ]];then
+    /usr/local/php8.1/bin/php think tools --action setVersion --newVersion "${big_v}.${medium_v}.${small_v} ${hash}"
+else
+    php think tools --action setVersion --newVersion "${big_v}.${medium_v}.${small_v} ${hash}"
+fi
